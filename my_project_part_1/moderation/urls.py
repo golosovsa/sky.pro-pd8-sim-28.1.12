9477cc409d7abea76b72adb92a2c9782c32148da
grm <golosov_SA@mail.ru>
2022-10-27 14:14:43 +0300

    moderation done

--- my_project_part_1/moderation/urls.py
+++ my_project_part_1/moderation/urls.py
@@ -1,3 +1,7 @@
 # TODO настраиваем urls здесь
+from django.urls import path
+from moderation import views
 
-urlpatterns = []
+urlpatterns = [
+    path("feedback-delete/<int:pk>/", views.ReviewDeleteView.as_view(), name="feedback-delete"),
+]
