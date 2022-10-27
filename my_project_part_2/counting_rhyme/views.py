8f8e2e493e60d95555e6e72233f9046bc30372d9
grm <golosov_SA@mail.ru>
2022-10-27 16:52:20 +0300

    counting_rhyme done

--- my_project_part_2/counting_rhyme/views.py
+++ my_project_part_2/counting_rhyme/views.py
@@ -1,11 +1,17 @@
 from django.http import JsonResponse
 from django.views import View
 
+from counting_rhyme.models import City, User
+
 
 # TODO внесите необходимые изменения в код ниже
 class CountView(View):
-    def get(self):
+    def get(self, request):
+
+        cities = City.objects.count()
+        users = User.objects.count()
+
         return JsonResponse({
-            "cities": "",
-            "users": "",
+            "cities": cities,
+            "users": users,
         })
